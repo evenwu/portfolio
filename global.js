12cf7b25a8b51022ef9ccb02754d551fed464f8c
Even Wu <wew062@ucsd.edu>
2026-04-17 16:00:58 -0700

    step 3 start

--- global.js
+++ global.js
@@ -4,10 +4,35 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$('nav a');
+// let navLinks = $$('nav a');
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
 
-currentLink?.classList.add("current");
+// currentLink?.classList.add("current");
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Projects' },
+  { url: 'resume/', title: 'Projects' },
+  { url: 'https://github.com/evenwu', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/protfolio/";         // GitHub Pages repo name
+
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
