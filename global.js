2568528360703e5ccc24bc56f0c0392e4c74c35c
Even Wu <wew062@ucsd.edu>
2026-04-17 16:54:08 -0700

    lab3 step4

--- global.js
+++ global.js
@@ -4,14 +4,6 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// let navLinks = $$('nav a');
-
-// let currentLink = navLinks.find(
-//   (a) => a.host === location.host && a.pathname === location.pathname,
-// );
-
-// currentLink?.classList.add("current");
-
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
@@ -32,7 +24,56 @@ const BASE_PATH = (location.hostname === "localhost" || location.hostname === "1
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
+
   url = !url.startsWith('http') ? BASE_PATH + url : url;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+    <label class="color-scheme">
+        Theme:
+        <select id="theme-select">
+            <option value="light dark">Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+    </label>`,
+);
+
+const select = document.querySelector('.color-scheme select');
+
+// Load saved preference
+if ("colorScheme" in localStorage) {
+  document.documentElement.style.colorScheme = localStorage.colorScheme;
+  select.value = localStorage.colorScheme;
+}
+
+// Listen for changes
+select.addEventListener('input', (event) => {
+  const value = event.target.value;
+
+  // Apply theme
+  document.documentElement.style.colorScheme = value;
+
+  // Save preference
+  localStorage.colorScheme = value;
+
+  console.log('color scheme changed to', value);
+});
+
+
 
